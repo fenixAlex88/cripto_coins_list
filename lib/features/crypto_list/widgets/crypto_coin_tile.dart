12afba13575783290286a3ff52356cde6c4e0202
Alex <fenix_al@tut.by>
2024-04-15 09:30:58 +0300

    lesson firebase

--- lib/features/crypto_list/widgets/crypto_coin_tile.dart
+++ lib/features/crypto_list/widgets/crypto_coin_tile.dart
@@ -1,6 +1,5 @@
 import 'package:crypto_coins_list/repositories/crypto_coins/models/crypto_coin.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class CryotoCoinTile extends StatelessWidget {
   const CryotoCoinTile({
@@ -14,7 +13,7 @@ class CryotoCoinTile extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return ListTile(
-      leading: Image.network(coin.imageUrl),
+      leading: Image.network(coin.imageUrl), 
       title: Text(
         coin.name,
         style: theme.textTheme.bodyMedium,
